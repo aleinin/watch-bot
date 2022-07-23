1c68b433c4041d9addb06c85b8cfd748c252ffd3
aleinin <aleininger.cs@gmail.com>
2022-07-22 22:56:45 -0500

    Update to shinier versions, fix breaking change

--- src/features/clear.ts
+++ src/features/clear.ts
@@ -1,13 +1,22 @@
-import {ClientUser, Message} from 'discord.js'
-import {ReturnReason} from '../on-message'
+import { ClientUser, Message } from 'discord.js'
+import { ReturnReason } from '../on-message'
 
-export const clearBotMessages = async (message: Message, author: ClientUser): Promise<ReturnReason> =>
-    message.delete()
-        .then(() => message.channel.messages
-            .fetch({limit: 100})
-            .then((fetchedMessages) =>
-                fetchedMessages.array().filter(fetchMsg => fetchMsg.author.equals(author))
-            )
+export const clearBotMessages = async (
+  message: Message,
+  author: ClientUser
+): Promise<ReturnReason> =>
+  message
+    .delete()
+    .then(() =>
+      message.channel.messages
+        .fetch({ limit: 100 })
+        .then((fetchedCollection) =>
+          Array.from(fetchedCollection.values()).filter((fetchedMessage) =>
+            fetchedMessage.author.equals(author)
+          )
         )
-        .then((messagesToDelete) => messagesToDelete.forEach((message) => message.delete()))
-        .then(() => ReturnReason.Success)
+    )
+    .then((messagesToDelete) =>
+      messagesToDelete.forEach((message) => message.delete())
+    )
+    .then(() => ReturnReason.Success)
